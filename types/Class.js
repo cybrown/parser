1fbe9f35ba61de4c423dfa87eb195f574de0de6c
Cy Brown <cy@cybrown.fr>
2013-12-31 22:27:46 +0100

    Implemting methods for Class and Interface classes

--- types/Class.js
+++ types/Class.js
@@ -1,11 +1,42 @@
 'use strict';
 
 var Type = require('./Type');
+var Interface = require('./Interface');
 
-var Class = function (superClass) {
+var Class = function () {
     Type.call(this);
+    this.parent = null;
+    this.interfaces = [];
 };
 Class.prototype = Object.create(Type.prototype);
+
+Class.prototype.isA = function (type) {
+    if (type === this) {
+        return true;
+    }
+    if (this.parent && this.parent.isA(type)) {
+        return true;
+    }
+    for (var i in this.interfaces) {
+        if (this.interfaces[i].isA(type)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+Class.prototype.extends = function (superclass) {
+    if (!(superclass instanceof Class)) {
+        throw new Error('Super class must be a class');
+    }
+    this.parent = superclass;
+};
+
+Class.prototype.implements = function (inter) {
+    if (this.interfaces.indexOf(inter) === -1) {
+        this.interfaces.push(inter);
+    }
+};
 
 module.exports = Class;
 
--- types/Interface.js
+++ types/Interface.js
@@ -4,7 +4,32 @@ var Type = require('./Type');
 
 var Interface = function () {
     Type.call(this);
+    this.interfaces = [];
 };
 Interface.prototype = Object.create(Type.prototype);
+
+Interface.prototype.implements = function (inter) {
+    if (!(inter instanceof Interface)) {
+        throw new Error('Interface must be an interface');
+    }
+    if (this.interfaces.indexOf(inter) === -1) {
+        this.interfaces.push(inter);
+    }
+};
+
+Interface.prototype.isA = function (inter) {
+    if (!(inter instanceof Interface)) {
+        return false;
+    }
+    if (this === inter) {
+        return true;
+    }
+    for (var i in this.interfaces) {
+        if (this.interfaces[i].isA(inter)) {
+            return true;
+        }
+    }
+    return false;
+};
 
 module.exports = Interface;
