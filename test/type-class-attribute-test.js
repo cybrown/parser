0d05a74c07f90e1ebeae1df76489388e550d2a28
Cy Brown <cy@cybrown.fr>
2014-01-01 19:30:17 +0100

    Added test for attributes and classes

---
+++ test/type-class-attribute-test.js
@@ -0,0 +1,12 @@
+describe ('Attributes and classes', function () {
+
+    it ('should add an attribute to a class', function () {
+        throw new Error('Test not implemented');
+    });
+
+    it ('should find if a class has an attribute', function () {
+        throw new Error('Test not implemented');
+    });
+
+
+});
