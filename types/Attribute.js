abc059063427b7c58ba0475104cb7195840a74b2
Cy Brown <cy@cybrown.fr>
2014-01-01 19:25:02 +0100

    Implemented Attribute class

--- types/Attribute.js
+++ types/Attribute.js
@@ -1,8 +1,34 @@
 'use strict';
 
-var Attribute = function (type, value) {
-    this.type = type;
-    this.value = value;
+var Type = require('./Type');
+
+var Attribute = function () {
+    this.type = null;
+    this.value = null;
 };
+
+Object.defineProperty(Attribute.prototype, 'name', {
+    get: function () {
+        return this._name
+    },
+    set: function (value) {
+        if (value !== null && typeof value !== 'string') {
+            throw new Error('Attribute#name must be a string.')
+        }
+        this._name = value;
+    }
+});
+
+Object.defineProperty(Attribute.prototype, 'type', {
+    get: function () {
+        return this._type;
+    },
+    set: function (value) {
+        if (value !== null && !(value instanceof Type)) {
+            throw new Error('Attribute#name must be a Type.')
+        }
+        this._type = value;
+    }
+});
 
 module.exports = Attribute;
