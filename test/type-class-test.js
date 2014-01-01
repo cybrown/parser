da94d5b3591a8f17c2a4ba8db087c9bbd8f059f0
Cy Brown <cy@cybrown.fr>
2014-01-01 19:24:25 +0100

    More tests and more implemented tests for type system

--- test/type-class-test.js
+++ test/type-class-test.js
@@ -28,14 +28,7 @@ describe ('Class', function () {
         c.isA(c).should.be.ok;
     });
 
-    it ('should be a sub class of a parent', function () {
-        var parent = new Class();
-        var c = new Class();
-        c.extends(parent);
-        c.isA(parent).should.be.ok;
-    });
-
-    it ('should be a sub class of a parent', function () {
+    it ('should extend another class', function () {
         var parent1 = new Class();
         var parent2 = new Class();
         parent1.extends(parent2);
@@ -45,7 +38,7 @@ describe ('Class', function () {
         parent2.isA(c).should.not.be.ok;
     });
 
-    it ('should be an implemented interface', function () {
+    it ('should be an interface implemented by a superclass', function () {
         var parent1 = new Class();
         var parent2 = new Class();
         var inter = new Interface();
@@ -55,5 +48,48 @@ describe ('Class', function () {
         c.extends(parent1);
         c.isA(inter).should.be.ok;
         inter.isA(c).should.not.be.ok;
+    });
+
+    it ('should not extend multiple times the same class', function () {
+        var c = new Class();
+        var cc = new Class();
+        c.extends(cc);
+        (function () {
+            c.extends(cc);
+        }).should.throw();
+    });
+
+    it ('should not extend a subtype', function () {
+        var c = new Class();
+        var c1 = new Class();
+        c.extends(c1);
+        (function () {
+            c1.extends(c);
+        }).should.throw();
+    });
+
+    it ('should calculate the distance with itself', function () {
+        var c1 = new Class();
+        c1.distance(c1).should.eql(0);
+    });
+
+    it ('should calculate the distance with a parent type', function () {
+        var c1 = new Class();
+        var c2 = new Class();
+        c1.extends(c2);
+        c1.distance(c2).should.eql(1);
+    });
+
+    it ('should only return distance with a class or an interface', function () {
+        var c1 = new Class();
+        var o = {};
+        c1.distance(o).should.eql(-1);
+    });
+
+    it ('should return -1 when another type is not a subtype', function () {
+        var c1 = new Class();
+        var c2 = new Class();
+        c1.extends(c2);
+        c2.distance(c1).should.eql(-1);
     });
 });
--- test/type-interface-test.js
+++ test/type-interface-test.js
@@ -9,17 +9,45 @@ describe ('Interface', function () {
         i.should.be.an.instanceOf(Type);
     });
 
-    it ('should add an interface', function () {
-        var i = new Interface();
-        var i2 = new Interface();
-        i.implements(i2);
-    });
-
     it ('should implement an interface', function () {
         var i = new Interface();
         var i2 = new Interface();
         i.implements(i2);
         i.isA(i2).should.be.ok;
         i2.isA(i).should.not.be.ok;
+    });
+
+    it ('should not implement an already implemented interface', function () {
+        var i = new Interface();
+        var i2 = new Interface();
+        i.implements(i2);
+        (function () {
+            i2.implements(i);
+        }).should.throw();
+    });
+
+    it ('should calculate the distance with itself', function () {
+        var i1 = new Interface();
+        i1.distance(i1).should.eql(0);
+    });
+
+    it ('should calculate the distance with a parent type', function () {
+        var i1 = new Interface();
+        var c2 = new Interface();
+        i1.extends(i2);
+        i1.distance(c2).should.eql(1);
+    });
+
+    it ('should only return distance with an interface', function () {
+        var i1 = new Interface();
+        var o = {};
+        i1.distance(o).should.eql(-1);
+    });
+
+    it ('should return -1 when another type is not a subtype', function () {
+        var i1 = new Interface();
+        var i2 = new Interface();
+        i1.extends(i2);
+        i2.distance(i1).should.eql(-1);
     });
 });
