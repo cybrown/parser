e59de3836a92e97f632a1164ab9b85e15eb32bb9
Cy Brown <cy@cybrown.fr>
2014-01-01 19:23:36 +0100

    Removed primitive type

--- types/index.js
+++ types/index.js
@@ -3,7 +3,6 @@
 var Type = require('./Type');
 var Class = require('./Class');
 var Interface = require('./Interface');
-var Primitive = require('./Primitive');
 var Attribute = require('./Attribute');
 var Method = require('./Method');
 
@@ -11,7 +10,6 @@ module.exports = {
     Type: Type,
     Class: Class,
     Interface: Interface,
-    Primitive: Primitive,
     Attribute: Attribute,
     Method: Method
 };
